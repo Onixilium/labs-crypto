fc6b2cbc2db68cd56a95c9d99a938e5aa8ecd038
Onixilium <16272969+Onixilium@users.noreply.github.com>
2020-02-14 09:17:07 +1000

    add transposition crypto

---
+++ coding_one/SimpleTransposition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codes1.coding_one
+{
+    class SimpleTransposition
+    {
+        public string Code(string str, string tabletranp)
+        {
+            str = " " + str;
+            tabletranp = " " + tabletranp;
+            string code = "";
+
+            string[] array1 = new string[str.Length];
+
+            for (int i = 1; i < str.Length; i++)
+            {
+               array1[int.Parse(tabletranp[i].ToString())] = str[i].ToString(); 
+               
+            }
+
+            return String.Join("",array1);
+        }
+    }
+}
